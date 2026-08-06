1ee6393be452f33f717e71ca1738a9476f44bfdf
TamaraJohansen <tamaracastanedaj@gmail.com>
2026-08-06 02:36:45 -0600

    Correcciones para poder visualizar las imágenes guardadas en los modelos

--- Cafemania/Cafemania/settings.py
+++ Cafemania/Cafemania/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-MEDIA_URL='/img/'
-MEDIA_ROOT=BASE_DIR/'static/img'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/'static']
+STATICFILES_DIRS = [BASE_DIR/'static',]
--- Cafemania/Menu/models.py
+++ Cafemania/Menu/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Producto(models.Model):
     nombre=models.CharField(max_length=100)
     descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    precio_mediano = models.DecimalField(max_digits=6, decimal_places=2)
+    precio_grande = models.DecimalField(max_digits=6, decimal_places=2)
     imagen= models.ImageField(upload_to='productos/')
 
     def __str__(self):
